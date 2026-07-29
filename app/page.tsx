b962d9360e5b29b75458e0988c2d4a9710d07382
bookguks-sudo <bookguks@gmail.com>
2026-07-29 22:37:05 +0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1>КРЕМЕНИАДА</h1>
-      <p>Система готова к приему данных.</p>
-    </main>
-  );
-}
